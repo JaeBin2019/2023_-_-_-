318e2073efcfd0d518841e26c43d98c591f6f7c2
JaeBin <201924576@pusan.ac.kr>
2023-12-06 18:34:25 +0900

    game module

--- backup.v
+++ backup.v
@@ -2,7 +2,7 @@ module save_answer(
     input wire clk,
     input wire reset,
     input wire play_music,
-    input wire [3:0] cur_index,
+    input wire [3:0] answer,
     input wire [31:0] data_in,
     input wire [3:0] max_index,
     input wire write_enable,
@@ -23,8 +23,8 @@ module save_answer(
         end
     end
 
-    assign click = (ticker < 5000000) ? 1'b1 : 1'b0; // Generates a high pulse every 0.1 second
 
+    assign click = (ticker == 5000000) ? 1'b1 : 1'b0; 
     reg [31:0] register;
     reg [3:0] max_index_reg;
     reg [3:0] auto_index;
@@ -32,8 +32,18 @@ module save_answer(
     reg is_music_playing;
     reg [3:0] piezo_reg;
     reg [3:0] data_reg;
+    reg [3:0] answer_index;
 
-    always @(posedge clk or posedge reset or posedge play_music) begin
+    /*
+        auto index 와 max index 가 같으면, 음악 재생을 멈추고 index를 0으로 바꾼다
+        if (auto_index == max_index_reg) {
+            auto_index <= 0;
+            is_music_playing <= 0;
+        }
+
+
+    */
+    always @(posedge clk or posedge reset or posedge play_music or posedge answer) begin
         if (reset) begin
             register <= 0;
             click_detected <= 0;
@@ -45,9 +55,9 @@ module save_answer(
         else if (play_music && !is_music_playing) begin
             auto_index <= 0;
             is_music_playing <= 1;
-            click_detected <= 0;
+            click_detected <= 1;
         end
-        else if (click && is_music_playing) begin
+        else if (click_detected && is_music_playing) begin
             // Output register[3:0] for 1 second
             case(auto_index)
             0 : 
@@ -68,35 +78,59 @@ module save_answer(
             3 : 
             begin
                 piezo_reg <= register[15:12];
+                if (auto_index == max_index_reg) {
+                    auto_index <= 0;
+                    is_music_playing <= 0;
+                }
                 auto_index <= auto_index + 1;
             end
             4 : 
             begin
                 piezo_reg <= register[19:16];
+                if (auto_index == max_index_reg) {
+                    auto_index <= 0;
+                    is_music_playing <= 0;
+                }
                 auto_index <= auto_index + 1;
             end
             5 : 
             begin
                 piezo_reg <= register[23:20];
+                if (auto_index == max_index_reg) {
+                    auto_index <= 0;
+                    is_music_playing <= 0;
+                }
                 auto_index <= auto_index + 1;
             end
             6 : 
             begin
                 piezo_reg <= register[27:24];
+                if (auto_index == max_index_reg) {
+                    auto_index <= 0;
+                    is_music_playing <= 0;
+                }
                 auto_index <= auto_index + 1;
             end
             7 : 
             begin
                 piezo_reg <= register[31:28];
-                auto_index <= 0;
-                is_music_playing <= 0;
+                if (auto_index == max_index_reg) {
+                    auto_index <= 0;
+                    is_music_playing <= 0;
+                }
             end
             endcase
-            click_detected <= 1;
-        end else if (!click) begin
-            click_detected <= 0;
+        
+    
+        // click_detected 를 1초마다 뒤집어서, 다시 위의 노래 재생 코드가 1초동안 실행되도록 한다
+        // detected 되지 않을 때는, piezo 를 끈다
+        end else if (click && is_music_playing) begin
+            click_detected <= !click_detected;
+            if (click_detected == 0) {
+                piezo_reg <= 0;
+            }
         end else begin
-            case(cur_index)
+            case(answer_index)
             0 : 
             begin
                 piezo_reg <= register[3:0];
