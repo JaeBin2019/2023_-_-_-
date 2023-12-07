3870065b04be85da3e63449263b99257bfb31118
JaeBin <201924576@pusan.ac.kr>
2023-12-07 14:52:26 +0900

    random 수정

--- random.v
+++ random.v
@@ -2,18 +2,37 @@
 // lcg.v
 //  Linear Congruential Generator PRNG
 // Default parameters taken from glibc
-module random #(parameter N=32, a=1103515245, c=12345) (
-    input clk50M,
-    output reg [N-1:0] rand
+module random #(parameter a=1103515245, c=12345) (
+    input clk,
+    input change_answer,
+    output reg [31:0] rand,
+    output write_enable
     );
     
     initial rand = 1; // I think this seed is good enough
     
     reg [N-1:0] next_rand;
+    reg change_answer_flag;
+    reg write_enable_reg;
     
     always @ (*)
-        next_rand = a * rand + c;
+        next_rand[3:0] = (a * rand + c) % 8 + 1;
+        next_rand[7:4] = (a * rand + c) % 8 + 1;
+        next_rand[11:8] = (a * rand + c) % 8 + 1;
+        next_rand[15:12] = (a * rand + c) % 8 + 1;
+        next_rand[19:16] = (a * rand + c) % 8 + 1;
+        next_rand[23:20] = (a * rand + c) % 8 + 1;
+        next_rand[27:24] = (a * rand + c) % 8 + 1;
+        next_rand[31:28] = (a * rand + c) % 8 + 1;
     
-    always @ (posedge clk50M)
+    always @ (posedge clk or posedge change_answer)
         rand <= next_rand;
+        if (change_answer) begin
+            change_answer_flag <= 1;
+        end else if (change_answer_flag) begin
+            change_answer_flag <= 0;
+            write_enable_reg <= 1;
+        end else if (write_enable_reg) begin
+            write_enable_reg <= 0;
+        end
 endmodule
